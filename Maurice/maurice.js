7f35e9b213cc4e6bf1018a3782e673bf7e613129
Maurice1306 <kommaurice@gmail.com>
2026-08-19 19:08:31 +0200

    Canvas hinzugefügt

--- Maurice/maurice.js
+++ Maurice/maurice.js
@@ -37,3 +37,53 @@ prevButton.addEventListener("click", function() {
 });
 
 zeigeSlide(aktuellerIndex);
+
+/*Canvas Erstellung*/ 
+
+function zeichneHeiligtuemerSymbol(){
+    const canvas = document.getElementById("heiligtuemerCanvas");
+
+    if(!canvas){
+        return;
+    }
+
+    const ctx = canvas.getContext("2d");
+
+    const breite = canvas.width;
+    const hoehe = canvas.height;
+
+    ctx.clearRect(0, 0, breite, hoehe);
+
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "#d3a625";
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+
+    ctx.shadowColor = "rgba(211, 166, 37, 0.8)";
+    ctx.shadowBlur = 18;
+
+    /*Dreieck zeichnen*/
+
+    ctx.beginPath();
+    ctx.moveTo(160, 35);
+    ctx.lineTo(45, 275);
+    ctx.lineTo(275, 275);
+    ctx.closePath();
+    ctx.stroke();
+
+    /*Kreis zeichnen*/
+
+    ctx.beginPath();
+    ctx.arc(160, 203, 69, 0, Math.PI * 2);
+    ctx.stroke();
+
+    /*Senkrechte zeichnen*/
+
+    ctx.beginPath();
+    ctx.moveTo(160, 35);
+    ctx.lineTo(160, 275);
+    ctx.stroke();
+
+}
+
+zeichneHeiligtuemerSymbol();
