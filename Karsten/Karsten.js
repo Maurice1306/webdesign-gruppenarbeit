bb330559320a4ca2a77fe21b617a1e1d370a0f73
PitPinguin23 <karsten.modrack@gmail.com>
2026-09-21 13:45:32 +0200

    Add files via upload

--- Karsten/Karsten.js
+++ Karsten/Karsten.js
@@ -21,14 +21,16 @@ function drawTable() {
     ctx.fillText("Doppelkopf", 400, 205);
 
     // Spieler
-    drawPlayer(400, 45, "Spieler 1");
+    drawPlayer(400, 55, "Spieler 1");
     drawPlayer(735, 210, "Spieler 2");
-    drawPlayer(400, 375, "Spieler 3");
+    drawPlayer(400, 365, "Spieler 3");
     drawPlayer(65, 210, "Spieler 4");
 
     // Karten im Stich
-    drawCard(335, 175, "♥");
-    drawCard(430, 175, "♠");
+    drawCard(400, 170, "♥", 0);
+    drawCard(450, 200, "♠", 90);
+    drawCard(400, 250, "♦", 180);
+    drawCard(350, 200, "♣", 270);
 }
 
 function drawPlayer(x, y, label) {
@@ -46,16 +48,35 @@ function drawPlayer(x, y, label) {
     ctx.fillText(label, x, y + 6);
 }
 
-function drawCard(x, y, symbol) {
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(x, y, 55, 70);
-    ctx.strokeStyle = "#1d2925";
-    ctx.strokeRect(x, y, 55, 70);
+function drawCard(x, y, symbol, angle) {
+    ctx.save();
 
-    ctx.fillStyle = symbol === "♥" ? "#b51f35" : "#1d2925";
+    //Mittelpunkt der Karte    
+    ctx.translate(x, y);
+
+    // Karte drehen
+    ctx.rotate(angle * Math.PI / 180);
+
+    // Schatten
+    ctx.fillStyle = "rgba(0, 0, 0, 0.18)";
+    ctx.fillRect(-23, -31, 50, 70);
+
+    // Karte
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(-27, -35, 54, 70);
+
+    ctx.strokeStyle = "#1d2925";
+    ctx.lineWidth = 2;
+    ctx.strokeRect(-27, -35, 54, 70);
+
+    // Symbol
+    ctx.fillStyle = (symbol === "♥" || symbol === "♦") ? "#b51f35" : "#1d2925";
     ctx.font = "32px Arial";
     ctx.textAlign = "center";
-    ctx.fillText(symbol, x + 27, y + 45);
+    ctx.textBaseline = "middle";
+    ctx.fillText(symbol, 0, 0);
+
+    ctx.restore();
 }
 
 drawTable();
